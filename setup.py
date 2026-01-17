914328026898820701a1a5bf9e2adb4f42e162ce
Sumanta Jyoti <sumantajyoti795@gmail.com>
2026-01-17 11:16:02 +0530

    Virtual Environment Created - requirements installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sumanta Jyoti",
+    author_email="sumantajyoti795@gmail.com",
+    packages=find_packages()
+)
